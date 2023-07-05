f5e59dfa13ae973241ccfe30527510e179d98267
gbg4812 <guillembaldi@gmail.com>
2023-07-05 17:27:06 +0200

    QThread worker aproach in the render manager, the worker and the thread are members of the manager

--- renderers/render_manager.py
+++ renderers/render_manager.py
@@ -1,8 +1,9 @@
-from typing import List, Dict
+from __future__ import annotations
+from typing import List, Dict, NoReturn, Any
 from subprocess import Popen, PIPE
 import json
 
-from PySide2.QtCore import Signal, QObject, QThread, QModelIndex, Slot
+from PySide2.QtCore import QRunnable, QThreadPool, Signal, QObject, QModelIndex, Slot
 from global_enums import TaskStates, DataRoles
 
 
@@ -13,7 +14,7 @@ class RenderManager(QObject):
     def __init__(self, parent=None) -> None:
         super(RenderManager, self).__init__(parent)
         self.is_rendering = False
-        self.renderThread = QThread()
+        self.thread_pool = QThreadPool()
         self.task_list: List[dict] = []
 
     def render(self, task_list: list) -> None:
@@ -22,9 +23,8 @@ class RenderManager(QObject):
             self.is_rendering = True
             renderWorker = RenderWorker(task_list)
             renderWorker.progress_updated.connect(self.progressUpdate)
-            renderWorker.moveToThread(self.renderThread)
-            self.renderThread.finished.connect(self.renderFinished)
-            self.renderThread.start()
+            renderWorker.finished.connect(self.renderFinished)
+            self.thread_pool.start(renderWorker)
         else:
             pass
 
@@ -35,17 +35,19 @@ class RenderManager(QObject):
         self.is_rendering = False
 
 
-class RenderWorker(QObject):
+class RenderWorker(QObject, QRunnable):
+    finished = Signal()
     progress_updated = Signal(dict)
 
-    def __init__(self, task_list: list = None, parent=None):
-        super(RenderWorker, self).__init__(parent)
-        self.task_list: List[dict] = task_list
+    def __init__(self, task_list: list):
+        super(RenderWorker, self).__init__()
+        self.task_list: List[Dict] = task_list
 
+    @Slot()
     def run(self) -> None:
         for bundle in self.task_list:
             dependent = bundle.get("dependent")
-            indexes: List[QModelIndex] = bundle.get("indexes")
+            indexes: List[QModelIndex] = bundle["indexes"]
             success = True
             for index in indexes:
                 index.model().setData(index, TaskStates.WAITING, DataRoles.TASKSTATE)
@@ -70,14 +72,18 @@ class RenderWorker(QObject):
                             index, TaskStates.FAILED, DataRoles.TASKSTATE
                         )
 
-    def renderTask(self, task: dict):
-        script: str = task.get("shell_script")
+    def renderTask(self, task: Dict[str, str]):
+        script = task["shell_script"]
         prog, arg = script.split(" ")
         task_str = json.dumps(task)
         subp = Popen([prog, arg, task_str], stdout=PIPE)
 
         objdata = dict()
         while True:
+            if self.kill_render:
+                subp.kill()
+                break
+
             data = subp.stdout.readline()
             data = data.rstrip(b"\r\n")
 
@@ -93,7 +99,8 @@ class RenderWorker(QObject):
 
         return 1 - subp.returncode
     
-    @Slot
+    @Slot()
     def killRender(self):
         self.kill_render = True
+
 
